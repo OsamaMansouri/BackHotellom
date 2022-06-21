1be29560f2204be9f1051912708f7ac65ec6861e
Osama Mansouri <mansouri.osama@gmail.com>
2022-06-21 13:55:55 +0100

    TETS

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/*testdddt*/
+/*testdddfdfdft*/
 
 Route::get('/portailCaptive', function () {
     return view("portail_captive");
