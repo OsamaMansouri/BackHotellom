e61cf83d3b9fb6e8006a919cca3b52b662b3d3e8
oussama <oussama.aitboulahna@gmail.com>
2023-01-06 15:32:34 +0100

    generate pdf

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -2,12 +2,17 @@
 <html>
 <head>
     <title>Qrcode</title>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap">
 </head>
 <style>
     body{
         background: #35143B;
         text-align: center;
-        font-family: "Times New Roman", Times, serif;
+        font-family: 'Montserrat', sans-serif;
+        margin: 0px; 
+    }
+    .content{
+        margin-top: 150px;
     }
     h2,h4,h3{
         color: #fff;
@@ -19,11 +24,6 @@
     h2{
         font-size: 45px;
     }
-    h4{
-        margin-top: -25px;
-        font-weight: 300;
-        font-size: 25px;
-    }
     h3{
         font-size: 22px;
     }
@@ -33,14 +33,20 @@
     .qrcode{
         width: 300px;
     }
+    .hotel_code{
+        font-size: 32px;
+    }
+    @page { margin: 0px; }
 </style>
 <body>
-    <img class="logo" src="/logohotellom.png"  alt="">
-    <h3>HOTEL IN YOUR POCKET</h3>
-    <h3>HOTEL CODE : {{$hotel_code}} </h3>
-    <img class="qrcode" src="https://api.hotellom.com/img/hotels/hotel-{{$reference}}.png">
-    <h3>APP.HOTELLOM.COM</h3>
-    <h1>SAVE UP TP 25%</h1>
-    <h4>SCAN THE QR CODE OR USE THE LINK <br> TO ACCESS THE APP</h4>
+    <div class="content">
+        <img class="logo" src="https://apptest.hotellom.com/static/media/logohotellom.png"  alt="">
+        <h3>HOTEL IN YOUR POCKET</h3>
+        <h3 class="hotel_code">HOTEL CODE : {{$hotel_code}} </h3>
+        <img class="qrcode" src="https://api.hotellom.com/img/hotels/hotel-{{$reference}}.png">
+        <h3>APP.HOTELLOM.COM</h3>
+        <h1>SAVE UP TP 25%</h1>
+        <h3>SCAN THE QR CODE OR USE THE LINK <br> TO ACCESS THE APP</h3>
+    </div>
 </body>
 </html>
